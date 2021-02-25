bd91e334b20f16784f7a8e3b826b878154c80769
vincent <vincbuchholz@gmail.com>
2021-02-25 12:38:08 +0100

    Create Oops.java
    taskSix

---
+++ Torsdag25Feb/TaskSix/Oops.java
@@ -0,0 +1,31 @@
+/*
+ *
+ * Hvis du kompilerer programmet får du 7 fejl.
+ * Kan du fixe programmet så det kan køre og
+ * outputtet bliver:
+ * "7 is the smallest!"
+ *
+ * 
+ */
+public class Oops {
+
+
+    public static void main(String[] args) {
+        int a = 7, b = 42;
+
+        minimum(a,b);
+
+        if (minimum(a,b) == a) {
+            System.out.println("a is the smallest!");
+        }
+    }
+    public static int minimum(int a, int b) {
+        int smaller = 0;
+        if (a < b) {
+            smaller = a;
+        } else if (a >= b) {
+            smaller = b;
+        }
+        return smaller;
+    }
+}
